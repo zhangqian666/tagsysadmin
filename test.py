e31138a085c73744a4d7a58e3e09e7540f926bbd
Zhang Qian <415677303@qq.com>
2022-03-02 10:29:26 +0800

    change ip

--- test.py
+++ test.py
@@ -8,11 +8,11 @@
 @Created on: 2021-04-18 10:55
 """
 
-# from add_templete_model import qa
+from add_templete_model import qa
 import json
 
 with open("src/question_answer_data.txt", encoding="utf-8", mode="r") as f:
     for i in f.readlines():
         line = json.loads(i)
         print("{} {}".format(line["question"], line["answer"]))
-        # qa.add_template_by_ner(line["question"], line["question"])
+        qa.add_template_by_ner(line["question"], line["question"])

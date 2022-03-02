acf2e2ce3357a5f1b24aa091f5c602b956f16875
Zhang Qian <415677303@qq.com>
2022-03-02 11:22:11 +0800

    change ip

--- test.py
+++ test.py
@@ -15,4 +15,4 @@ with open("src/question_answer_data.txt", encoding="utf-8", mode="r") as f:
     for i in f.readlines():
         line = json.loads(i)
         print("{} {}".format(line["question"], line["answer"]))
-        qa.add_template_by_ner(line["question"], line["question"])
+        qa.add_template_by_ner(line["question"], line["answer"])
